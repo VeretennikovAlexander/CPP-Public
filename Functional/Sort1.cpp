25308681506ed09b0c9b2584400b6a70674f7bce
Alexander Veretennikov <alexander@veretennikov.org>
2025-04-11 22:23:59 +0500

    Commit message

--- Functional/Sort1.cpp
+++ Functional/Sort1.cpp
@@ -31,10 +31,19 @@ string ToString(const V& v)
     return result;
 };
 
+template <class T>
+bool Compare(T a, T b)
+{
+	return a < b;
+}
+
 int main(int argc, const char** args)
 {
  vector<float> Data({0,5,1,7,2});
 
+ /*
+  * Сортировка с помощью лямбда функции.
+  */ 
  sort(Data.begin(), Data.end(), [](const decltype(Data)::value_type a, const decltype(Data)::value_type b) {
     return a < b;
  });
@@ -44,6 +53,29 @@ int main(int argc, const char** args)
  for_each(Data.begin(), Data.end(), [](decltype(Data)::value_type Val){
 	 cout << Val << endl;
 	 });
+	 
+	 
+	 
+ /*
+  * Сортировка с помощью вспомогательного класса-функции.
+  */	  
+	 
+	 
+ struct CSortOperator
+ {
+	 bool operator()(const decltype(Data)::value_type a, const decltype(Data)::value_type b)
+	 {
+		 return a < b;
+	 }
+ };
+ 	 
+ sort(Data.begin(), Data.end(), CSortOperator());
+ 
+ /*
+  * Сортировка с помощью обычной функции.
+  */ 
+ sort(Data.begin(), Data.end(), Compare<decltype(Data)::value_type>); 
+	 
 	 
  return 0;
 }
